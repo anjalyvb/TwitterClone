d0ac73886ad34a9dfe81ea3d80e7a6dc952719b2
Admin <Admin@DOTNET>
2018-12-04 09:51:49 +0530

    Importing project

---
+++ TwitterClone/Models/Dashboard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TwitterClone.Models
+{
+    public class DashboardModel
+    {
+        public string LoggedInUserId { get; set; }
+        public int TweetCount { get; set; }
+        public int Following { get; set; }
+        public int Followers { get; set; }
+        public string Message { get; set; }
+        public virtual Person Person { get; set; }
+        public virtual List<Tweet> TweetList { get; set; }
+    }
+}
---
+++ TwitterClone/Models/Tweet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TwitterClone.Models
+{
+    public class Tweet
+    {
+        public int TweetId { get; set; }
+        [Required]
+        public string TweetUser { get; set; }
+        [Required]
+        public string TweetMessage { get; set; }
+        public string TweetTime { get; set; }
+    }
+}
